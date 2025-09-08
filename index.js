cc3cb52bc2da185f921a9e9457ad45dd7f8ba1a4
ABHI1996ABHI <abhishekroks12345@gmail.com>
2025-09-08 23:52:36 +0530

    first logic

---
+++ index.js
@@ -0,0 +1,16 @@
+
+
+function addone(){
+    const homeScore = document.getElementById('home-score');
+    homeScore.textContent = parseInt(homeScore.textContent) + 1;
+}
+
+function addtwo(){
+    const homeScore = document.getElementById('home-score');
+    homeScore.textContent = parseInt(homeScore.textContent) + 2;
+}
+
+function addthree(){
+    const homeScore = document.getElementById('home-score');
+    homeScore.textContent = parseInt(homeScore.textContent) + 3;
+}
