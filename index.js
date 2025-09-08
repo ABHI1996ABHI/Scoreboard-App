d0d0cee01754270ac52b372d76c9a1b086ed7372
ABHI1996ABHI <abhishekroks12345@gmail.com>
2025-09-09 00:32:21 +0530

    final commit

--- index.js
+++ index.js
@@ -14,3 +14,34 @@ function addthree(){
     const homeScore = document.getElementById('home-score');
     homeScore.textContent = parseInt(homeScore.textContent) + 3;
 }
+
+
+function guestone(){
+       const  guestScore = document.getElementById('guest-score');
+       guestScore.textContent = parseInt(guestScore.textContent) + 1;
+
+}
+
+function guesttwo(){
+
+    const  guestScore = document.getElementById('guest-score');
+    guestScore.textContent = parseInt(guestScore.textContent) + 2;
+
+}
+
+function guestthree(){
+
+    const  guestScore = document.getElementById('guest-score');
+    guestScore.textContent = parseInt(guestScore.textContent) + 2;
+
+}
+
+function reset(){
+
+    const homeScore = document.getElementById('home-score');
+    homeScore.textContent = 0;
+    const guestScore = document.getElementById('guest-score');
+    guestScore.textContent = 0;
+
+    
+}
